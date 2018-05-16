012d19692c10a75660905008a160b83f9039a8a3
Hawladar Roman <hawladar.roman@bjitgroup.com>
2018-05-16 11:35:09 +0600

    lare

--- app/src/main/java/com/dreampany/todo/injector/ActivityModule.java
+++ app/src/main/java/com/dreampany/todo/injector/ActivityModule.java
@@ -9,7 +9,7 @@ import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 
 @Module
-public abstract class BuildersModule {
+public abstract class ActivityModule {
     @ActivityScoped
     @ContributesAndroidInjector
     abstract LaunchActivity launchActivity();
